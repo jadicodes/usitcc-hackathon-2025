0944a28fe524e26e665ccea92259f3cb676bab37
Jadi Miller <jadilmiller@gmail.com>
2025-03-28 16:59:09 -0400

    Change rocks to buttons

--- project/minigames/firepit/rock.gd
+++ project/minigames/firepit/rock.gd
@@ -1,30 +1,22 @@
-extends Node2D
+extends Button
 
 var draggable = false
 var is_inside_dropable = false
 var body_ref
 var offset: Vector2
 var inPos : Vector2 #Initial Posistion
+var grabbed: bool
 
 signal rock_placed
 
-
 func _process(delta):
-	if draggable:
-		if Input.is_action_just_pressed("click"):
-			inPos = global_position
-			offset = get_global_mouse_position() - global_position
-			global.is_dragging = true
-		if Input.is_action_pressed("click"):
-			global_position = get_global_mouse_position() - offset
-		elif Input.is_action_just_released("click"):
-			global.is_dragging = false
-			var tween = get_tree().create_tween()
-			if is_inside_dropable:
-				tween.tween_property(self,"position", body_ref.position,0.2).set_ease(Tween.EASE_OUT)
-			else:
-				tween.tween_property(self, "global_position", inPos,0.2).set_ease(Tween.EASE_OUT)
-
+	if grabbed:
+		var mouse_pos = get_global_mouse_position()
+		global_position = mouse_pos
+		global.is_dragging = true
+		var tween = get_tree().create_tween()
+		if is_inside_dropable:
+			tween.tween_property(self,"position", body_ref.position,0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered():
 	if not global.is_dragging:
@@ -47,4 +39,16 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = false
+
+func _unhandled_input(event):
+	if draggable and (event is InputEventMouseButton or event is InputEventMouseMotion):
+		get_viewport().set_input_as_handled()
 		
+
+
+func _on_button_down() -> void:
+	grabbed = true
+
+
+func _on_button_up() -> void:
+	grabbed = false
