faa38cb7ba2d8470798763f9b5035149f2d812f5
Jamison8 <jamisontuckerwork@gmail.com>
2025-03-28 15:31:14 -0400

    Add end scene

--- project/minigames/firepit/rock.gd
+++ project/minigames/firepit/rock.gd
@@ -23,7 +23,7 @@ func _process(delta):
 			else:
 				tween.tween_property(self, "global_position", inPos,0.2).set_ease(Tween.EASE_OUT)
 	if winCond == 6:
-		get_tree().quit() #used to quit the game to test a win condition
+		get_tree().change_scene_to_file("res://end_scene.tscn")
 
 
 func _on_area_2d_mouse_entered():
