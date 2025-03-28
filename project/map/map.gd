9ee040b6359736fc9fe3a2bd51866fc3aac98083
Jadi Miller <jadilmiller@gmail.com>
2025-03-28 18:35:11 -0400

    Add Michigan village

--- project/map/map.gd
+++ project/map/map.gd
@@ -13,3 +13,15 @@ func _on_village_button_2_pressed() -> void:
 
 func get_village(button):
 	return button.village
+
+
+func _on_village_button_3_pressed() -> void:
+	village_selected.emit($VillageButton3)
+
+
+func _on_village_button_4_pressed() -> void:
+	village_selected.emit($VillageButton4)
+
+
+func _on_village_button_5_pressed() -> void:
+	village_selected.emit($VillageButton5)
