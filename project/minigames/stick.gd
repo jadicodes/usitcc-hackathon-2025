a032f60956568bf652ee21d31ad9921ce1648dd1
Jadi Miller <jadilmiller@gmail.com>
2025-03-28 14:18:17 -0400

    Make stick only move if you press the button

--- project/minigames/stick.gd
+++ project/minigames/stick.gd
@@ -1,19 +1,37 @@
 extends Line2D
 
 @export var num_segments: int = 4
-@export var segment_length: int = 2
-@export var starting_pos: Vector2 = Vector2(100, 00)
-
+@export var segment_length: int = 80
+var grabbed : bool
 
 
 func _process(_delta):
-	var point_count = get_point_count()
-	
-	var mouse_pos = to_local(get_global_mouse_position())
-	set_point_position(0, mouse_pos)
+	if grabbed:
+		var mouse_pos = to_local(get_global_mouse_position())
+		set_point_position(0, mouse_pos)
+		$Button.position = mouse_pos
+
+		# Make each point follow the one before it
+		for i in range(1, get_point_count() - 1):
+			var prev_pos = get_point_position(i - 1)
+			var current_pos = get_point_position(i)
+			var direction = (prev_pos - current_pos).normalized()
+			var target_pos = prev_pos - direction * segment_length
+			
+			# Prevent overlap by keeping a fixed distance
+			if current_pos.distance_to(prev_pos) > segment_length:
+				set_point_position(i, target_pos)
 
 
 func _draw():
 	for point_index in get_point_count():
 		var point_position: Vector2 = get_point_position(point_index)
 		draw_circle(point_position, 20, Color.SADDLE_BROWN)
+
+
+func _on_button_button_down() -> void:
+	grabbed = true
+
+
+func _on_button_button_up() -> void:
+	grabbed = false
