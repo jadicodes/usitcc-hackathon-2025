c09abaf0fdc0544e391245bc52fe352adf026cb9
Jadi Miller <jadilmiller@gmail.com>
2025-03-28 00:12:17 -0400

    Add background and textures to resource

--- project/map/village_button.gd
+++ project/map/village_button.gd
@@ -3,7 +3,13 @@ extends TextureButton
 var village_name : String
 
 @export var village : Village
+@onready var label_text = %Name
+
+func _ready() -> void:
+	set_properties()
+
 
 func set_properties():
 	texture_normal = village.button_texture
-	village_name = village.village_name
+	label_text.text = village.village_name
+	
