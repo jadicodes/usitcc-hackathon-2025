e5d40890b5c41961953a2d6986ad1eeaa3d2566f
Jamison8 <jamisontuckerwork@gmail.com>
2025-03-28 14:46:22 -0400

    Add color to placeholder rocks and fire

--- project/minigames/rock.gd
+++ project/minigames/rock.gd
@@ -37,6 +37,7 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group('dropable'):
 		is_inside_dropable = true
 		body_ref = body
+		
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
