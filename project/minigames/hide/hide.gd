902ef8d5c5c6d1af4a9036996bf47397377e01e7
Jamison8 <jamisontuckerwork@gmail.com>
2025-03-28 20:03:41 -0400

    Update hide to work with the stage

--- project/minigames/hide/hide.gd
+++ project/minigames/hide/hide.gd
@@ -3,13 +3,13 @@ extends Node2D
 
 var secured : int = 0
 
-signal suceeded
+signal succeeded
 
 func _process(delta):
 	
 	if secured != 4:
 		return
-	emit_signal("suceeded")
+	emit_signal("succeeded")
 
 
 func _on_tapping_button_finished():
